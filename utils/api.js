c9bd8bd011dac426f17d916bab460f59d92d8301
Andre Peregrina <andre.peregrina@gmail.com>
2020-07-27 13:40:08 -0700

    Fix: problem with uuid library and Expo

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,5 @@
 import {AsyncStorage} from 'react-native';
-import 'react-native-get-random-values';
-import {v4 as uuidv4} from 'uuid';
-import {parseToJSON} from "./index";
+import {parseToJSON, uuidv4} from "./index";
 
 const STORAGE_KEY = "MobileFlashCards:decks";
 
